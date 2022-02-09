94194a122750ca57f19c4a67bb3f3f9abc9c0b18
Craig Ramsay <craig.ramsay.100@strath.ac.uk>
2022-02-09 13:06:13 +0000

    Nix integration for build and tests

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
 srcs := $(shell find src/ -type f -name '*.idr')
 bin := ./build/exec/toatie
 
-.PHONY : all bin test
+.PHONY : all bin check install
 
-all : $(bin) test
+all : $(bin) check
 
 $(bin): $(srcs) toatie.ipkg
 	idris2 --build toatie.ipkg
 
-test: $(bin)
+bin: $(bin)
+
+check: $(bin)
 	make -C Test -k all
+
+install: $(bin)
+	mkdir -p $(out)/bin
+	cp -r build/exec/* $(out)/bin
 
 clean:
 	rm -r build
--- flake.nix
+++ flake.nix
@@ -1,26 +1,54 @@
 {
-  description = "A flake for building Hello World";
+  description = "toatie --- a toy language for hardware description with dependent types";
 
   inputs.flake-utils.url = github:numtide/flake-utils;
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  inputs.idris2-dev.url = github:idris-lang/Idris2/v0.5.1;
+  inputs.nixpkgs.url     = github:NixOS/nixpkgs;
+  inputs.idris.url       = github:idris-lang/Idris2/v0.5.1;
 
-  outputs = { self, flake-utils, nixpkgs, idris2-dev }:
+  outputs = { self, flake-utils, nixpkgs, idris }:
     flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
 
-		  shellHook = ''
-			export PATH=$PWD/build/exec:$PATH
-			${idris2-dev.packages.${system}.idris-emacs}/bin/emacs --eval "(require 'idris2-mode)" &
+      let
+
+        pkgs       = import nixpkgs { inherit system; };
+        idrisPkgs  = idris.packages.${system};
+        toatiePkg = pkgs.stdenv.mkDerivation ({
+          name = "toatie";
+          src = ./.;
+          doCheck = true;
+          nativeBuildInputs = [ idrisPkgs.idris2 ];
+          buildPhase = ''
+            make bin
+          '';
+	        checkPhase = ''
+		        export PATH=$PWD/build/exec:$PATH
+            make check
+          '';
+          installPhase = ''
+            make install
+          '';
+        });
+
+      in {
+
+        packages = { toatie = toatiePkg; };
+        defaultPackage = toatiePkg;
+
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            export PATH=$PWD/build/exec:$PATH
+            alias devEmacs="${idris.packages.${system}.idris-emacs}/bin/emacs --eval \"(require \'idris2-mode)\" &"
+            alias devTest="(cd $PWD; make check)"
+            alias devBuild="(cd $PWD; make bin)"
+            alias devRepl="$PWD/build/exec/toatie"
           '';
 
           buildInputs = [
-            (idris2-dev.packages.${system}.idris2)
-            (idris2-dev.packages.${system}.emacs-with-idris)
+            (idris.packages.${system}.idris2)
+            (idris.packages.${system}.emacs-with-idris)
           ];
         };
+
       }
     );
 }
